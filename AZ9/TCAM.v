4a0c401b70c83e491c42e10988c283335264d9f5
mohammadrezaabdi <wnick2085@gmail.com>
2021-05-14 23:43:05 +0430

    final touches

--- AZ9/TCAM.v
+++ AZ9/TCAM.v
@@ -1,64 +1,67 @@
-module TCAM (word,address,R_Wb,clk);
-  parameter word_length = 8;
-  parameter address_length = 5;
-  inout [address_length - 1 : 0] address;
-  input [word_length - 1 : 0] word;
-  input R_Wb ,clk;
-  reg [word_length - 1 : 0] memory [(1 << address_length) - 1 : 0];
-  reg [address_length: 0] i;
-  reg [address_length - 1 : 0] readCandidate;
-
-  assign address = (R_Wb) ? readCandidate : 'bz;
-
-  // this function get number of x elements in given bit-vector
-  function [word_length - 1 : 0] numberOfX;
-    input [word_length - 1 : 0] c;
-    reg [word_length - 1 : 0] number, j;
-    begin
-      number = 0;
-      for(j = 0 ; j < word_length ; j = j + 1) begin
-        if (c[j] === 1'bx) begin
-          number = number + 1;
+module TCAM (word,
+             address,
+             rwn,
+             clk);
+    parameter WORD_LEN = 8;
+    parameter ADDR_LEN = 5;
+    
+    inout [ADDR_LEN - 1 : 0] address;
+    input [WORD_LEN - 1 : 0] word;
+    input rwn ,clk;
+    
+    reg [WORD_LEN - 1 : 0] memory [(1 << ADDR_LEN) - 1 : 0];
+    reg [ADDR_LEN - 1 : 0] read_candidate;
+    
+    assign address = (rwn) ? read_candidate : 'bz;
+    
+    
+    reg [ADDR_LEN: 0] i;
+    always @ (posedge clk) begin
+        if (rwn) begin
+            read_candidate = 'bx;
+            for (i = 0 ; i < (1 << ADDR_LEN) ; i = i + 1) begin
+                if (equals(memory[i] , word)) begin
+                    if (read_candidate === 'bx || (number_of_x(memory[read_candidate]) > number_of_x(memory[i]))) begin
+                        read_candidate = i;
+                    end
+                end
+            end
         end
-      end
-    numberOfX = number;
-    end
-  endfunction
-
-  // this function checks equality of two bit-vectors with ignoring x's
-  function equals;
-    input [word_length - 1 : 0] a,b;
-    reg [word_length - 1 : 0] j;
-    reg flag;
-    begin
-      flag = 1;
-      for(j = 0 ; j < word_length ; j = j + 1) begin
-        if (a[j] !== 1'bx && b[j] !== 1'bx && a[j] != b[j]) begin
-          flag = 0;
+        else begin
+            memory[address] = word;
         end
-      end
-      equals = flag;
     end
-  endfunction
-
-  // read
-  always @ (posedge clk) begin
-    if (R_Wb) begin
-      readCandidate = 'bx;
-      for (i = 0 ; i < (1 << address_length) ; i = i + 1) begin
-        if(equals(memory[i] , word)) begin
-          if (readCandidate === 'bx || (readCandidate !== 'bx && numberOfX(memory[readCandidate]) > numberOfX(memory[i]))) begin
-            readCandidate = i;
-          end
+    
+    
+    // this function get number of x elements in given bit-vector
+    function [WORD_LEN - 1 : 0] number_of_x;
+        input [WORD_LEN - 1 : 0] c;
+        reg [WORD_LEN - 1 : 0] number, j;
+        begin
+            number = 0;
+            for(j = 0 ; j < WORD_LEN ; j = j + 1) begin
+                if (c[j] === 1'bx) begin
+                    number = number + 1;
+                end
+            end
+            number_of_x = number;
         end
-      end
-    end
-  end
-
-  // write
-  always @ (posedge clk) begin
-    if (!R_Wb) begin
-      memory[address] = word;
-    end
-  end
+    endfunction
+    
+    // this function checks equality of two bit-vectors with ignoring x's
+    function equals;
+        input [WORD_LEN - 1 : 0] a,b;
+        reg [WORD_LEN - 1 : 0] j;
+        reg flag;
+        begin
+            flag = 1;
+            for(j = 0 ; j < WORD_LEN ; j = j + 1) begin
+                if (a[j] !== 1'bx && b[j] !== 1'bx && a[j] != b[j]) begin
+                    flag = 0;
+                end
+            end
+            equals = flag;
+        end
+    endfunction
+    
 endmodule
--- AZ9/testbench.v
+++ AZ9/testbench.v
@@ -1,36 +1,53 @@
-module testbench ;
-  reg [7:0] word;
-  reg R_Wb,clk;
-  wire [1:0] address;
-  reg [1:0] address_help;
-
-  TCAM tcam0 (word,address,R_Wb,clk);
-  defparam tcam0.word_length = 8;
-  defparam tcam0.address_length = 2;
-
-  always #1 clk = ~clk; // clock cycle is 2 
-  assign address = (!R_Wb) ? address_help : 'bz;
-
-  initial begin
-        clk = 0;
-        R_Wb = 0; word = 8'b0111xx11; address_help = 0;
-    #2  R_Wb = 0; word = 8'b11x01001; address_help = 1;
-    #2  R_Wb = 0; word = 8'b01110x11; address_help = 2;
-    #2  R_Wb = 0; word = 8'b1101xx11; address_help = 3;
-    #2  R_Wb = 1; word = 8'b0111xx11;
-    #2  R_Wb = 1; word = 8'bxxxxxxxx;
-    #2  R_Wb = 1; word = 8'b11xxxx11;
-    #2  R_Wb = 0; word = 8'b11110101; address_help = 0;
-    #2  R_Wb = 0; word = 8'b101xxxxx; address_help = 1;
-    #2  R_Wb = 0; word = 8'b10110x11; address_help = 2;
-    #2  R_Wb = 0; word = 8'b0100xx01; address_help = 3;
-    #2  R_Wb = 1; word = 8'b11xx0101;
-    #2  R_Wb = 1; word = 8'b10011011;
-    #2  R_Wb = 1; word = 8'b10xxxxxx;
-    #2  $stop;
-  end
-
-  initial begin
-    $monitor($time," R/Wb=%d  word=%b address=%d",R_Wb,word,address);
-  end
+`define NULL 0
+module testbench();
+    
+    parameter WORD_LEN = 16;
+    parameter ADDR_LEN = 4;
+    
+    parameter clk_c = 10;
+    reg rwn, clk;
+    reg [WORD_LEN-1:0] word;
+    wire [ADDR_LEN-1:0] address;
+    reg [ADDR_LEN-1:0] w_addr;
+    
+    TCAM #(.WORD_LEN(WORD_LEN), .ADDR_LEN(ADDR_LEN)) tcam (
+    .word(word),
+    .address(address),
+    .rwn(rwn),
+    .clk(clk)
+    );
+    
+    assign address = (!rwn) ? w_addr : 'bz;
+    
+    initial begin
+        $dumpfile("report/waveform.vcd");
+        $dumpvars(0,tcam);
+    end
+    
+    initial begin
+        clk         = 0;
+        forever clk = #(clk_c/2) ~clk;
+    end
+    
+    initial
+        $monitor($time," rwn = %d  word = %b address = %d",rwn,word,address);
+    
+    initial begin
+        #clk_c  rwn = 0; word = 16'b0111xx11_0111xx11; w_addr = 0;
+        #clk_c  rwn = 0; word = 16'b11x01001_11x01001; w_addr = 1;
+        #clk_c  rwn = 0; word = 16'b01110x11_01110x11; w_addr = 2;
+        #clk_c  rwn = 0; word = 16'b1101xx11_1101xx11; w_addr = 3;
+        #clk_c  rwn = 1; word = 16'b0111xx11_0111xx11;
+        #clk_c  rwn = 1; word = 16'bxxxxxxxx_xxxxxxxx;
+        #clk_c  rwn = 1; word = 16'b11xxxx11_11xxxx11;
+        #clk_c  rwn = 0; word = 16'b11110101_11110101; w_addr = 0;
+        #clk_c  rwn = 0; word = 16'b101xxxxx_101xxxxx; w_addr = 1;
+        #clk_c  rwn = 0; word = 16'b10110x11_10110x11; w_addr = 2;
+        #clk_c  rwn = 0; word = 16'b0100xx01_0100xx01; w_addr = 3;
+        #clk_c  rwn = 1; word = 16'b11xx0101_11xx0101;
+        #clk_c  rwn = 1; word = 16'b10011011_10011011;
+        #clk_c  rwn = 1; word = 16'b10xxxxxx_10xxxxxx;
+        #clk_c  $finish;
+    end
+    
 endmodule
